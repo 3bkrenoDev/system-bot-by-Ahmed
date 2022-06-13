bb14889c480676f9cd1e2657d4d21545738069dd
3bkrenoDev <107380235+3bkrenoDev@users.noreply.github.com>
2022-06-13 07:37:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   token:process.env.token,
-  globalBot:false,
-  guildID:"941383131242561537",
+  globalBot:false, // false => if bot is private bot - true => if bot isn't private bot
+  guildID:"Server ID",
 }
