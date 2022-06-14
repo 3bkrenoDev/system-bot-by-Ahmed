846b00f3c5aa73f1cbf1c0f98b300de0a11a934b
3bkrenoDev <107380235+3bkrenoDev@users.noreply.github.com>
2022-06-14 08:26:05 +0200

    Update help.js

--- SlashCommands/General/help.js
+++ SlashCommands/General/help.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js")
+let { splitMessage } = require("../../Functions/utils.js")
 
 module.exports = {
   name:`help`,
@@ -7,9 +8,24 @@ module.exports = {
   botperms:["EMBED_LINKS"],
   cooldown:5,
   run:async(client, interaction,args) => {
-    let commands = client.slashCommands.map(c => `\`${c.name}\``)
+    let commands = client.slashCommands.map(command =>
+      `**● | /${command.name}** - **${command.description}**`).join("\n")
+
     let embed = new MessageEmbed()
-    .setDescription(commands.join(", "))
-    interaction.reply({embeds:[embed]}).catch(err => 0)
+    .setTitle(`Help Menu`) // By Ahmed Abd El-Latif Gaming
+    .setAuthor({name:`${interaction.user.tag}`,
+     iconURL:interaction.user.avatarURL()})
+    .setColor(`RED`)
+    .setThumbnail(client.user.avatarURL())
+    .setTimestamp()
+    .setFooter({text:`This Bot Made By Ahmed Abd El-Latif Gaming`});
+    let split = splitMessage(`${commands}\n`,{
+      char:"\n",
+      maxLength:1950
+    })
+    split.map((s,i) => {
+      embed.setDescription(s)
+      i == 0 ? interaction.reply({embeds:[embed]}).catch(err=>0) : interaction.channel.send({embeds:[embed]}).catch(err=>0)
+      })  
   }
 }
