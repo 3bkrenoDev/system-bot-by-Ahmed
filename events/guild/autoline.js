14ba2231cb3688e8dbf7370aeb952ceefa9a34b1
3bkrenoDev <107380235+3bkrenoDev@users.noreply.github.com>
2023-05-24 06:23:02 +0300

    Update autoline.js

--- events/guild/autoline.js
+++ events/guild/autoline.js
@@ -1,18 +1,16 @@
 var guildModel = require("../../DataBase/models/guild.js")
 const Discord = require("discord.js")
 module.exports = {
-	name: 'messageCreate',
-  run:async(message,client)=> {
+  name: 'messageCreate',
+  run: async (message, client) => {
     let globalBot = client.config.globalBot
     let ID = client.config.guildID
-    if(message.author.id ===client.user.id || (!globalBot && message.guild.id !== ID)) return;
-    let newData = await guildModel.findOne({guildID:message.guild.id}) 
-    if(!newData || !newData.autoLinesChannel.length)return;
+    if (message.author.id === client.user.id || (!globalBot && message.guild.id !== ID)) return;
+    let newData = await guildModel.findOne({ guildID: message.guild.id })
+    if (!newData || !newData.autoLinesChannel.length) return;
     let obj = newData.autoLinesChannel.find(c => c.channelId === message.channel.id)
-    if(!obj) return;
-    var utf8encoded = (Buffer.from(obj.line, 'base64'))
-
-    let att = new Discord  .MessageAttachment(utf8encoded ,"line.gif")
-    message.channel.send({files:[att]}).catch(err => 0)
+    if (!obj) return;
+    let att = new Discord.MessageAttachment(obj.line, "line.gif")
+    message.channel.send({ files: [att] }).catch(err => 0)
   }
 }
